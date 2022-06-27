c0c6716eaeef3ca0636cc4710cc7c0c7b1b5a4f4
Pratik More <portfolio@pratikmore.com>
2022-06-27 04:20:25 -0400

    Columns added
    Columns added for navigation and google ads

--- chatwithPM/resources/views/chat.blade.php
+++ chatwithPM/resources/views/chat.blade.php
@@ -1,14 +1,25 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <div class="card">
-        <div class="card-header">Chats</div>
-        <div class="card-body">
-            <chat-messages :messages="messages"></chat-messages>
+    <div class="row">
+        <div class="col-sm">
+            navigation
         </div>
-        <div class="card-footer">
-            <chat-form v-on:messagesent="addMessage" :user="{{ Auth::user() }}"></chat-form>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">Ask Questions on PHP Problems </div>
+                <div class="card-body">
+                    <chat-messages :messages="messages"></chat-messages>
+                </div>
+                <div class="card-footer">
+                    <chat-form v-on:messagesent="addMessage" :user="{{ Auth::user() }}"></chat-form>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            Google Ads
         </div>
     </div>
+    
 </div>
 @endsection
